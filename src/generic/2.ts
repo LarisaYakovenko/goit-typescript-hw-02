b2430c724f3420dbd1231dbf89f0fba9112cc826
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2023-12-30 20:11:24 +0200

    update tasks

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,11 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+type PicTop = Pick < AllType,'name' | 'color' >;
+type PickBotton = Pick<AllType, 'position' | 'weight' >;
+
+
+function compare (top: any, bottom: any): AllType {
   return {
     name: top.name,
     color: top.color,
